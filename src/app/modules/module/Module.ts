3772ddab2db908797fa3e8d89cbe245b1937b795
Damjan Jelaš <damjan.jelas@t-matix.com>
2016-05-16 10:00:23 +0200

    Typings JSON

--- src/app/modules/module/Module.ts
+++ src/app/modules/module/Module.ts
@@ -1,5 +1,9 @@
 /// <reference path="module.interface.ts" />
 
+
+/**
+ * Module class documentation
+ */
 export default class ModuleClass implements ModuleClassInterface {
     value:any;
     method(value: any): any {
